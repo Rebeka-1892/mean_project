730663dd2511d3f075300994560d79c60c5db20e
RAVALISON <ravalisonjeremie@gmail.com>
2025-03-19 23:58:55 +0300

    Correction service

--- frontend/src/app/services/login.service.ts
+++ frontend/src/app/services/login.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@angular/core';
-import {Service} from './service';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class LoginService extends Service {
-  constructor(protected override http: HttpClient) {
-    super(http);
-    this.apiUrl = `${this.apiUrl}/login`;
-  }
+export class LoginService {
+  protected apiUrl = `${environment.apiUrl}/login`;
+  constructor(protected http: HttpClient) { }
 
   login(user: any): Observable<any> {
     return this.http.post(`${this.apiUrl}`, user);
--- frontend/src/app/services/service.ts
+++
@@ -1,7 +0,0 @@
-import {environment} from '../../environments/environment';
-import {HttpClient} from '@angular/common/http';
-
-export class Service {
-  protected apiUrl = `${environment.apiUrl}`;
-  constructor(protected http: HttpClient) { }
-}
