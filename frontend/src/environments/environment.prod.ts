8aafffde1fbc563cb41653ebbd576139d802ee55
RAVALISON <ravalisonjeremie@gmail.com>
2025-03-23 20:43:31 +0300

    Déploiement

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.monsite.com'  // URL pour la production 
+    apiUrl: 'https://api.monsite.com'  // URL pour la production
+    // apiUrl: 'https://meanproject-production.up.railway.app'  // URL pour le déploiement
 };
