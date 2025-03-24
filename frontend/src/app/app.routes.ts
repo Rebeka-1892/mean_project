205614a584d50fd06a505025d3cd8b056b62f798
RAVALISON <ravalisonjeremie@gmail.com>
2025-03-25 00:15:33 +0300

    Layouts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import {LoginComponent} from './components/login/login.component';
+import {LoginComponent} from './layouts/login-layout/login-layout.component';
 //new_import
 export const routes: Routes = [
     // { path: '', redirectTo: 'login', pathMatch: 'full' }  // Redirection  par défaut
-    { path: '', component: LoginComponent }
-    //new_path
+    { path: '**', redirectTo: 'login' },
+    { path: 'login', component: LoginComponent }
 ];
