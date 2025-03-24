37f304e85ab0b531df83179b87f0f766228f52fd
RAVALISON <ravalisonjeremie@gmail.com>
2025-03-25 00:34:16 +0300

    Layouts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { routes } from './app.routes';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     provideHttpClient(withFetch())
