daee6b3aed78690c5f5a67cc08e0445474b86d7a
RAVALISON <ravalisonjeremie@gmail.com>
2025-03-27 13:42:47 +0300

    Correction middleware

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ const app = express();
 const PORT = process.env.PORT || 5000; 
 
 // Middleware 
-app.use(cors()); 
+app.use((req, res, next) => {
+    if (req.path === '/login') {
+        cors()(req, res, next);
+    } else {
+        cors({ origin: process.env.FRONTEND_URL, credentials: true })(req, res, next);
+    }
+});
 app.use(express.json());
 app.use(cookieParser());
 
