1114a24019b4762fc549ef580cede7f33e427d48
RAVALISON <ravalisonjeremie@gmail.com>
2025-03-27 15:11:02 +0300

    Correction Layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,15 +3,20 @@ import {LoginComponent} from './layouts/login-layout/login-layout.component';
 import { RoleListComponent } from './components/role/role-list.component';
 import { RoleCreateComponent } from './components/role/role-create.component';
 import { RoleEditComponent } from './components/role/role-edit.component';
+import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
 //new_import
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirection  par défaut,
-    { path: 'login', component: LoginComponent },
-    { path: 'roles-edit/:id', component: RoleEditComponent },
-    { path: 'roles-create', component: RoleCreateComponent },
-    { path: 'roles', component: RoleListComponent },
-    //new_path
-
-    // url pour les URLs inconnues
-    { path: '**', redirectTo: 'login' } // Redirection  par défaut,
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirection par défaut
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: 'roles-edit/:id', component: RoleEditComponent },
+      { path: 'roles-create', component: RoleCreateComponent },
+      { path: 'roles', component: RoleListComponent },
+    ]
+  },
+  // URL pour les URLs inconnues
+  { path: '**', redirectTo: 'login' } // Redirection par défaut
 ];
