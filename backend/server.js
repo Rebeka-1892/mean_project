06e57aeb7b610cd48ae36126b4f5a372560ef3b0
RAVALISON <ravalisonjeremie@gmail.com>
2025-03-29 12:06:30 +0300

    Gestion d'acces

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000; 
 
 // Middleware 
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true, exposedHeaders: ['X-Redirect'] }));
 app.use(express.json());
 app.use(cookieParser());
 
