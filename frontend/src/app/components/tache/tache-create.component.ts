e4a952da0054f44dbc0731211aa5be385fc9dc81
RAVALISON <ravalisonjeremie@gmail.com>
2025-04-04 02:14:28 +0300

    Correction tache

--- frontend/src/app/components/tache/tache-create.component.ts
+++ frontend/src/app/components/tache/tache-create.component.ts
@@ -5,130 +5,130 @@ import { FactureService } from '../../services/facture.service';
 import { EmployeService } from '../../services/employe.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import {catchError, of, switchMap} from 'rxjs';
-import {RoleService} from '../../services/role.service';
-import BaseComponent from '../BaseComponent';
-import {FormuleroleService} from '../../services/formulerole.service';
-import {map} from 'rxjs/operators';
+import { forkJoin, of, switchMap } from 'rxjs';
+import { RoleService } from '../../services/role.service';
+import { FormuleroleService } from '../../services/formulerole.service';
+import { map } from 'rxjs/operators';
 
 @Component({
-    selector: 'app-tache-create',
-    imports: [CommonModule, FormsModule],
-    templateUrl: './tache-create.component.html',
-    standalone: true
+  selector: 'app-tache-create',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './tache-create.component.html',
+  standalone: true
 })
-export class TacheCreateComponent extends BaseComponent implements OnInit {
-    list: any;
-    employes: any;
-    roles: any;
-    newTache = { idfacture: '', idrole: '', idemploye: [] as string[] };
+export class TacheCreateComponent implements OnInit {
+  factures: any;
+  roles: { role: any; employes: any[]; error?: string }[] = [];
+  newTache = { idfacture: '', idrole: '', idemploye: [] as { idrole: string, idemploye: string }[] };
 
-    constructor(
-        private tacheService: TacheService,
-        private factureService: FactureService,
-        private employeService: EmployeService,
-        private roleService: RoleService,
-        private formuleroleService: FormuleroleService,
-        private router: Router
-    ) {
-      super();
-    }
+  constructor(
+    private tacheService: TacheService,
+    private factureService: FactureService,
+    private employeService: EmployeService,
+    private roleService: RoleService,
+    private formuleroleService: FormuleroleService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.tacheService.getTaches().pipe(
-      switchMap((taches: any[]) => {
-        const tacheFactureIds = taches.map((tache: any) => tache.idfacture);
-        return this.factureService.getFactures().pipe(
-          map((factures: any[]) => factures.filter((facture: any) => !tacheFactureIds.includes(facture._id)))
-        );
-      }),
-      switchMap((factures: any[]) => {
-        this.list = factures;
-        if (factures.length > 0) {
-          this.newTache.idfacture = factures[0]._id;
-          return this.roleService.getRoleByIdFacture(factures[0]._id);
+    this.factureService.getFactureSansTaches().pipe(
+      switchMap((data: any[]) => {
+        this.factures = data;
+        if (this.factures.length > 0) {
+          this.newTache.idfacture = this.factures[0]._id;
+          return this.roleService.getRoleByIdFacture(this.newTache.idfacture);
         } else {
-          return of([]);
+          return of([] as any[]);
         }
       }),
       switchMap((roles: any[]) => {
-        this.roles = roles;
         if (roles.length > 0) {
-          this.newTache.idrole = roles[0]._id;
-          return this.employeService.getEmployeByIdFactureAndByIdRole(this.newTache.idfacture, this.newTache.idrole);
+          const employeRequests = roles.map((role: any) =>
+            this.employeService.getEmployeByIdFactureAndByIdRole(this.newTache.idfacture, role._id).pipe(map(employes => ({ role, employes, error: '' })))
+          );
+          return forkJoin(employeRequests);
         } else {
-          return of([]);
+          return of([] as { role: any; employes: any[]; error: string }[]);
         }
-      }),
-      catchError(error => {
-        console.error(error);
-        return of([]);
       })
-    ).subscribe((employes: any[]) => {
-      this.employes = employes;
+    ).subscribe((roles: { role: any; employes: any[]; error: string }[]) => {
+      this.roles = roles;
+    }, (error) => {
+      console.error('Erreur lors de la récupération des données :', error);
     });
   }
 
-    toggleTache(id: string, event: Event) {
-        const inputElement = event.target as HTMLInputElement;
-        const isChecked = inputElement.checked;
-        if (isChecked) {
-            this.newTache.idemploye.push(id);
-        } else {
-            this.newTache.idemploye = this.newTache.idemploye.filter(serviceId => serviceId !== id);
-        }
-    }
-
-  addTache(): void {
-    if (this.newTache.idfacture && this.newTache.idrole && this.newTache.idemploye.length > 0) {
-      this.formuleroleService.getFormuleroleByIdFactureAndByIdRole(this.newTache.idfacture, this.newTache.idrole).pipe(
-        switchMap((formulerole) => {
-          const heure = formulerole[0].heure;
-          const nombre = formulerole[0].nombre;
-
-          if (nombre < this.newTache.idemploye.length) {
-            this.error = 'Le nombre d\'employés ne doit pas dépasser le nombre défini dans le rôle.';
-            return of(null); // Retourne un observable vide pour arrêter le flux
-          }
-
-          const requests = this.newTache.idemploye.map(id => {
-            console.log(`id: ${id}, idfacture: ${this.newTache.idfacture}, idrole: ${this.newTache.idrole}, heure: ${heure}`);
-            return this.tacheService.addTache({ idfacture: this.newTache.idfacture, idrole: this.newTache.idrole, idemploye: id, statut: 0, heure: heure }).toPromise();
-          });
-
-          return Promise.all(requests);
-        })
-      ).subscribe({
-        next: () => {
-          this.router.navigate(['/taches']);
-        },
-        error: (error) => {
-          console.error('Erreur lors de l’ajout des postes :', error);
-        }
-      });
+  toggleTache(idrole: string, idemploye: string, event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const isChecked = inputElement.checked;
+    if (isChecked) {
+      this.newTache.idemploye.push({ idrole, idemploye });
+    } else {
+      this.newTache.idemploye = this.newTache.idemploye.filter(e => !(e.idrole === idrole && e.idemploye === idemploye));
     }
   }
 
-    goBackToList() {
-        this.router.navigate(['/taches']);
-    }
+  addTache(): void {
+    let hasError = false;
 
-    onFactureChange() {
-        this.roleService.getRoleByIdFacture(this.newTache.idfacture).pipe(
-            switchMap(roles => {
-                this.roles = roles;
-                this.newTache.idrole = roles[0]._id;
-                return this.employeService.getEmployeByIdFactureAndByIdRole(this.newTache.idfacture, this.newTache.idrole);
-            }),
-            catchError(error => {
-                return of([]);
-            })
-        ).subscribe(employes => {
-            this.employes = employes;
-        });
-    }
+    this.roles.forEach((role) => {
+      const selectedEmployes = this.newTache.idemploye.filter(e => e.idrole === role.role._id);
+      this.formuleroleService.getFormuleroleByIdFactureAndByIdRole(this.newTache.idfacture, role.role._id).pipe(
+        switchMap((formulerole) => {
+          const heure = formulerole[0].heure;
+          const nombre = formulerole[0].nombre;
+          if (nombre !== selectedEmployes.length) {
+            role.error = `Le nombre d'employés ayant le rôle ${role.role.nom} doit être égal à ${nombre}.`;
+            hasError = true;
+            return of(null);
+          }
+          return of({ heure, role });
+        })
+      ).subscribe({
+        next: (result) => {
+          if (result && !hasError) {
+            const { heure, role } = result;
+            const requests = selectedEmployes.map(employe => {
+              return this.tacheService.addTache({ idfacture: this.newTache.idfacture, idrole: role.role._id, idemploye: employe.idemploye, statut: 0, heure: heure }).toPromise();
+            });
+            Promise.all(requests).then(() => {
+              this.router.navigate(['/taches']);
+            }).catch((error) => {
+              console.error('Erreur lors de l’ajout des postes :', error);
+            });
+          }
+        },
+        error: (error) => {
+          console.error('Erreur lors de la récupération des données :', error);
+        }
+      });
+    });
 
-    onRoleChange() {
-        this.employeService.getEmployeByIdFactureAndByIdRole(this.newTache.idfacture, this.newTache.idrole).subscribe((employes) => this.employes = employes);
+    if (hasError) {
+      console.error('Des erreurs ont été trouvées dans les rôles.');
     }
+  }
+
+  goBackToList() {
+    this.router.navigate(['/taches']);
+  }
+
+  onFactureChange() {
+    this.roleService.getRoleByIdFacture(this.newTache.idfacture).pipe(
+      switchMap((roles: any[]) => {
+        if (roles.length > 0) {
+          const employeRequests = roles.map((role: any) =>
+            this.employeService.getEmployeByIdFactureAndByIdRole(this.newTache.idfacture, role._id).pipe(map(employes => ({ role, employes, error: '' })))
+          );
+          return forkJoin(employeRequests);
+        } else {
+          return of([] as { role: any; employes: any[]; error: string }[]);
+        }
+      })
+    ).subscribe((roles: { role: any; employes: any[]; error: string }[]) => {
+      this.roles = roles;
+    }, (error) => {
+      console.error('Erreur lors de la récupération des données :', error);
+    });
+  }
 }
