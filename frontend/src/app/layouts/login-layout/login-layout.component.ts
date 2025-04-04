733b6c74d21c99f8a61aaf710c01083c81f031d2
RAVALISON <119033194+RAVALISON1404@users.noreply.github.com>
2025-04-04 10:05:33 +0300

    Correction de login

--- frontend/src/app/layouts/login-layout/login-layout.component.ts
+++ frontend/src/app/layouts/login-layout/login-layout.component.ts
@@ -29,10 +29,11 @@ import {jwtDecode} from 'jwt-decode';
     login(): void {
       this.loginService.login(this.utilisateur).subscribe(
         () => {
-          const token = this.cookieService.get('token');
-          const decodedToken: any = jwtDecode(token);
-          const url = decodedToken.role === 'manager' ? '/dashboard' : '/taches';
-          this.router.navigate([this.returnUrl ?? url]);
+          // const token = this.cookieService.get('token');
+          // const decodedToken: any = jwtDecode(token);
+          // const url = decodedToken.role === 'manager' ? '/dashboard' : '/taches';
+          // this.router.navigate([this.returnUrl ?? url]);
+          this.router.navigate([this.returnUrl ?? '/dashboard']);
         },
         error => {
           this.error = error.error.message;
