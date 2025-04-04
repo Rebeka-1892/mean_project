4cfce308483530abe196bf32f154ef1ff061ea2f
RAVALISON <ravalisonjeremie@gmail.com>
2025-04-04 08:39:04 +0300

    Correction

--- backend/server.js
+++ backend/server.js
@@ -9,23 +9,20 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    exposedHeaders: ["X-Redirect"],
-  })
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+        exposedHeaders: ["X-Redirect"],
+    })
 );
 app.use(express.json());
 app.use(cookieParser());
 
 // Connexion à MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connecté"))
-  .catch((err) => console.log(err));
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connecté"))
+    .catch((err) => console.log(err));
 
 // Routes
 app.use("/menus", require("./routes/menuRoutes"));
