de0fc44daba2a4978b4b489d34edc096927f7137
RAVALISON <ravalisonjeremie@gmail.com>
2025-04-04 08:24:06 +0300

    Correction

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -12,6 +12,7 @@ const authMiddleware = (rolesAutorises = []) => {
 
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      console.log('token decoded', decoded);
       // req.user = decoded;
       
       if (rolesAutorises.length && !rolesAutorises.includes(decoded.role)) {
